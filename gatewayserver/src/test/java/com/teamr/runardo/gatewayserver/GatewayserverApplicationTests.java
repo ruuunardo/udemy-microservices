134746e5a3173255dda86a1e58f631f85dc843f0
ruuunardo <114326970+ruuunardo@users.noreply.github.com>
2024-10-01 09:08:07 +0800

    s9-added gateway server

---
+++ gatewayserver/src/test/java/com/teamr/runardo/gatewayserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamr.runardo.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
